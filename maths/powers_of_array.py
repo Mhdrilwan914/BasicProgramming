96c616aba4ad1b13b4443787a1a9a7ad0e48a7d3
mohammed Riwan <mhdrilwan914@gmail.com>
2022-04-15 01:15:27 +0530

    problem for power of array

---
+++ maths/powers_of_array.py
@@ -0,0 +1,20 @@
+from array import array
+
+
+def main():
+    a=[]
+    n=int (input("enter the size: "))
+    b=[1]*n
+    pow_num=int(input("enter the power: "))
+    for i in range(0,n):
+        j=int(input("Enter array elements -{}: ".format(i)))
+        a.append(j)
+    print(a)
+    for x in range(0,n):
+        for k in range(pow_num):
+            b[x]=b[x]*a[x]
+    
+    print(b) 
+
+main()
+
